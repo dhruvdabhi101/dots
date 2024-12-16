723fcaf24cc9c21ee15b7d892db534a99933e92e
Dhruv Dabhi <dhruvdabhi@Dhruvs-MacBook-Air.local>
2024-12-16 19:28:43 +0530

    updated

--- neovimrc/init.lua
+++ neovimrc/init.lua
@@ -3,20 +3,20 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
-local client = vim.lsp.start_client {
-  name = "darkdownlsp",
-  cmd = { "darkdownlsp" }
-}
+-- local client = vim.lsp.start_client {
+--   name = "darkdownlsp",
+--   cmd = { "darkdownlsp" }
+-- }
 
-if not client then
-  vim.notify("Do the client thing right")
-  return
-end
+-- if not client then
+  --vim.notify("Do the client thing right")
+  -- return
+-- end
 
-vim.api.nvim_create_autocmd({ "BufRead", "FileType" }, {
-  pattern = "*.dd",
-  callback = function()
-    vim.lsp.buf_attach_client(0, client)
-  end,
-})
-
+-- vim.api.nvim_create_autocmd({ "BufRead", "FileType" }, {
+--   pattern = "*.dd",
+--   callback = function()
+--     vim.lsp.buf_attach_client(0, client)
+--   end,
+-- })
+--
